b44ddfda6f9eb8a0c2e8e7391cc2d63b8cf3af53
KamiyamaShun <76637787+KamiyamaShun@users.noreply.github.com>
2021-02-04 11:54:23 +0900

    set btton

--- counterApp/ContentView.swift
+++ counterApp/ContentView.swift
@@ -8,9 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var number = 0
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+        ZStack{
+            Image("counter")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+            Text("\(number)")
+                .foregroundColor(.white)
+            }
+        Button(action: {self.number += 1}) {
+            Text("カウント")
+             }
+            Button(action: {self.number = 0}) {
+                Text("リセット")
+                    .padding()
+            }
+        }
     }
 }
 
