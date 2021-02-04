b2591a463f5e0260a1242d99ffc54ac3c1b05fae
KamiyamaShun <76637787+KamiyamaShun@users.noreply.github.com>
2021-02-04 11:14:47 +0900

    Initial Commit

---
+++ counterApp/counterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  counterAppApp.swift
+//  counterApp
+//
+//  Created by 神山駿 on 2021/02/04.
+//
+
+import SwiftUI
+
+@main
+struct counterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
